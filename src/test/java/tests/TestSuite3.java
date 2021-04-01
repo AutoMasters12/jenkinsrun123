bfd0bafd24aac35efb07a19b6023043721897bd6
suman <suman143learning@gmail.com>
2021-04-01 20:03:02 +0530

    first commit

---
+++ src/test/java/tests/TestSuite3.java
@@ -0,0 +1,26 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSuite3 {
+
+	@Test
+	public void test1() {
+		System.out.println("test01");
+	}
+	@Test
+	public void test2() {
+		System.out.println("test02");
+	}
+	@Test
+	public void test3() {
+		System.out.println("test03");
+	}
+	@Test
+	public void test4() {
+		System.out.println("test04");
+	}
+
+}
